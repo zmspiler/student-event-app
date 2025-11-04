fee38ecbf5efeb0cdaa065ebdca2ec238a6f6ab4
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-04 10:07:24 +0100

    Add logger to hono

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,13 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import { auth } from "./auth";
 import { handler } from "./handler";
 
 const app = new Hono();
+
+app.use(logger());
 
 app.use(
   "*",
