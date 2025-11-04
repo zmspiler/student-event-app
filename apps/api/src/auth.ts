75e15dbfd8a485ae46e6a6aebf5931a3bef92763
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-04 10:04:34 +0100

    Add OpenAPI spec for auth endpoints

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -1,8 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+  plugins: [openAPI({
+    path: '/spec'
+  })],
   database: prismaAdapter(prisma, {
     provider: "sqlite",
   }),
