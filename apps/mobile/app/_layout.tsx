e692bbe85ca58c7d9f05737de5b89acfc1a1a640
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-05 09:40:04 +0100

    Hide header

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -3,5 +3,11 @@ import "../global.css";
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }
