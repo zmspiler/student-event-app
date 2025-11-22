2c0a1e46b82c1396c97210f3f753ccb5f3ec5b4e
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-22 13:19:44 +0100

    Add field validation

--- apps/mobile/app/login.tsx
+++ apps/mobile/app/login.tsx
@@ -1,44 +1,78 @@
-import { router, useRouter } from "expo-router";
-import { useState } from "react";
-import { Button, TextInput, View } from "react-native";
+import { useForm } from "@tanstack/react-form";
+import { useRouter } from "expo-router";
+import { TextInput, View } from "react-native";
+import z from "zod";
+import { Button } from "@/components/button";
 import { authClient } from "@/lib/auth-client";
 
 export default function Login() {
+  const { Subscribe, Field, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onChange: z.object({
+        email: z.email(),
+        password: z.string().min(8),
+      }),
+    },
+    onSubmit: async ({ value: { email, password } }) => {
+      await authClient.signIn.email(
+        {
+          email,
+          password,
+        },
+        {
+          onSuccess: () => router.replace("/(tabs)/settings"),
+          onError: (ctx) => {
+            alert(ctx.error.message);
+            console.log(ctx);
+          },
+        },
+      );
+    },
+  });
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <View className="p-4 gap-2">
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        className="border border-gray-400 rounded-xl px-4"
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        className="border border-gray-400 rounded-xl px-4"
-        secureTextEntry
-      />
-      <Button
-        title="Login"
-        onPress={async () => {
-          await authClient.signIn.email(
-            {
-              email,
-              password,
-            },
-            {
-              onSuccess: () => router.replace("/(tabs)/settings"),
-              onError: (ctx) => {
-                console.log(ctx);
-              },
-            },
-          );
-        }}
+      <Field name="email">
+        {(field) => (
+          <TextInput
+            placeholder="Email"
+            className="border border-gray-400 rounded-xl px-4"
+            value={field.state.value}
+            onChangeText={field.handleChange}
+          />
+        )}
+      </Field>
+
+      <Field name="password">
+        {(field) => (
+          <TextInput
+            placeholder="Password"
+            className="border border-gray-400 rounded-xl px-4"
+            value={field.state.value}
+            onChangeText={field.handleChange}
+            secureTextEntry
+          />
+        )}
+      </Field>
+
+      <Subscribe
+        selector={(state) => [
+          state.canSubmit,
+          state.isSubmitting,
+          state.isPristine,
+        ]}
+        children={([canSubmit, isSubmitting, isPristine]) => (
+          <Button
+            title="Login"
+            disabled={!canSubmit || isSubmitting || isPristine}
+            onPress={handleSubmit}
+          />
+        )}
       />
     </View>
   );
---
+++ apps/mobile/components/button.tsx
@@ -0,0 +1,21 @@
+import { Text, TouchableOpacity } from "react-native";
+
+export function Button({ title, disabled, onPress }: Props) {
+  return (
+    <TouchableOpacity
+      className={`w-100 p-3 bg-blue-400 rounded-xl${disabled ? " opacity-50" : ""}`}
+      disabled={disabled}
+      onPress={onPress}
+    >
+      <Text className={`mx-auto text-white${disabled ? " text-black" : ""}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+type Props = {
+  title: string;
+  disabled?: boolean;
+  onPress?: () => void | Promise<void>;
+};
