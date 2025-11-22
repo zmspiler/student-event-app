17fd6e5acea2b63cea8b4ee295c0a023bf3b6ebd
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-22 20:18:30 +0100

    Upgrade prisma to v7, add ready message

--- apps/api/src/prisma.ts
+++ apps/api/src/prisma.ts
@@ -1,3 +1,9 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
+import { DATABASE_URL } from "./environment";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaBetterSqlite3({
+  url: DATABASE_URL || "file:./prisma/dev.db",
+});
+
+export const prisma = new PrismaClient({ adapter });
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -49,7 +49,27 @@ app.on(["POST", "GET"], "/auth/*", (c) => {
   return auth.handler(c.req.raw);
 });
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port: 3000,
+});
+
+server.on("listening", () => {
+  console.log(`Server is ready.`);
+});
+
+// Handle graceful shutdown
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 });
