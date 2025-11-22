d5aecc0048d073e0a52fe300544e53bc314a45d2
Žiga Maj Špiler <zmspiler@pm.me>
2025-11-22 12:02:55 +0100

    Fix base url property name

--- apps/mobile/lib/auth-client.ts
+++ apps/mobile/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-  baseUrl: process.env.EXPO_PUBLIC_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   basePath: "/auth",
   plugins: [
     expoClient({
