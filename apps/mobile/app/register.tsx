2eb6b1160f26ad945d1236cdd741683b97d23282
Žiga Maj Špiler <zmspiler@pm.me>
2025-12-15 08:59:26 +0100

    Update register page styling

--- apps/mobile/app/register.tsx
+++ apps/mobile/app/register.tsx
@@ -1,54 +1,86 @@
-import { router, useRouter } from "expo-router";
-import { useState } from "react";
-import { Button, TextInput, View } from "react-native";
+import { useForm } from "@tanstack/react-form";
+import { useRouter } from "expo-router";
+import { TextInput, View } from "react-native";
+import z from "zod";
+import { Button } from "@/components/button";
 import { authClient } from "@/lib/auth-client";
 
 export default function Register() {
   const router = useRouter();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { Subscribe, Field, handleSubmit } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validators: {
+      onChange: z.object({
+        name: z.string().min(1),
+        email: z.email(),
+        password: z.string().min(8),
+      }),
+    },
+    onSubmit: async ({ value: { name, email, password } }) => {
+      await authClient.signUp.email(
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          onSuccess: () => router.replace("/(tabs)/settings"),
+        },
+      );
+    },
+  });
 
   return (
     <View className="p-4 gap-2">
-      <TextInput
-        value={name}
-        onChangeText={setName}
-        placeholder="Name"
-        className="border border-gray-400 rounded-xl px-4"
-      />
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        className="border border-gray-400 rounded-xl px-4"
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        className="border border-gray-400 rounded-xl px-4"
-        secureTextEntry
-      />
-      <Button
-        title="Register"
-        onPress={async () => {
-          await authClient.signUp.email(
-            {
-              name,
-              email,
-              password,
-            },
-            {
-              onError: (ctx) => {
-                console.log(ctx);
-              },
-              onSuccess: () => {
-                router.replace("/(tabs)");
-              },
-            },
-          );
-        }}
+      <Field name="name">
+        {(field) => (
+          <TextInput
+            placeholder="Name"
+            className="border border-gray-400 rounded-xl px-4"
+            value={field.state.value}
+            onChangeText={field.handleChange}
+          />
+        )}
+      </Field>
+      <Field name="email">
+        {(field) => (
+          <TextInput
+            placeholder="Email"
+            className="border border-gray-400 rounded-xl px-4"
+            value={field.state.value}
+            onChangeText={field.handleChange}
+          />
+        )}
+      </Field>
+      <Field name="password">
+        {(field) => (
+          <TextInput
+            placeholder="Password"
+            className="border border-gray-400 rounded-xl px-4"
+            value={field.state.value}
+            onChangeText={field.handleChange}
+            secureTextEntry
+          />
+        )}
+      </Field>
+
+      <Subscribe
+        selector={(state) => [
+          state.canSubmit,
+          state.isSubmitting,
+          state.isPristine,
+        ]}
+        children={([canSubmit, isSubmitting, isPristine]) => (
+          <Button
+            title="Register"
+            disabled={!canSubmit || isSubmitting || isPristine}
+            onPress={handleSubmit}
+          />
+        )}
       />
     </View>
   );
