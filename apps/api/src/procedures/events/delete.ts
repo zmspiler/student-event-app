a61c996a5c0df4d82bf4bdc2abf24efff2c964c8
Žiga Maj Špiler <zmspiler@pm.me>
2025-12-20 14:13:29 +0100

    Implement deletion on events

--- apps/api/src/procedures/events/delete.ts
+++ apps/api/src/procedures/events/delete.ts
@@ -1,12 +1,14 @@
 import { requireAuth } from "@/middleware/require-auth";
 import { prisma } from "@/utils/prisma";
 
-export default requireAuth.events.delete.handler(async ({ input, errors }) => {
-  const event = await prisma.event.delete({
-    where: { id: input.id },
-  });
+export default requireAuth.events.delete.handler(
+  async ({ input, errors, context: { session } }) => {
+    const event = await prisma.event.delete({
+      where: { id: input.id, ownerId: session.user.id },
+    });
 
-  if (!event) {
-    throw errors.NOT_FOUND();
-  }
-});
+    if (!event) {
+      throw errors.NOT_FOUND();
+    }
+  },
+);
