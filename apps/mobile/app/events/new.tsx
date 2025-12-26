b569de30d0a1c5d396b4f77bee377f090ae0c13f
Žiga Maj Špiler <zmspiler@pm.me>
2025-12-26 22:43:23 +0100

    Add screen title

--- apps/mobile/app/events/new.tsx
+++ apps/mobile/app/events/new.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { EventForm } from "@/forms/event-form";
 import { apiQueryClient } from "@/lib/api-client";
 import { mergeDateAndTime } from "@/lib/utils/date-time";
@@ -11,15 +11,18 @@ export default function NewEvent() {
   );
 
   return (
-    <EventForm
-      onSubmit={async ({ description, date, time, ...value }) => {
-        await mutateAsync({
-          description: description ?? "",
-          date: mergeDateAndTime(date, time),
-          ...value,
-        });
-        navigate("/(tabs)");
-      }}
-    />
+    <>
+      <Stack.Screen options={{ headerTitle: "New Event" }} />
+      <EventForm
+        onSubmit={async ({ description, date, time, ...value }) => {
+          await mutateAsync({
+            description: description ?? "",
+            date: mergeDateAndTime(date, time),
+            ...value,
+          });
+          navigate("/(tabs)");
+        }}
+      />
+    </>
   );
 }
