092a84706e07c9e5ef7702aff25b05de18a24331
Žiga Maj Špiler <zmspiler@pm.me>
2026-01-04 12:43:40 +0100

    Update prisma generation location

--- apps/api/src/utils/prisma.ts
+++ apps/api/src/utils/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { DATABASE_URL } from "./environment";
 
 const adapter = new PrismaBetterSqlite3({
