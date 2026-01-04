4c4a914c1e71b755d9ac605d0f49d5bbc8a3a34a
Žiga Maj Špiler <zmspiler@pm.me>
2026-01-04 12:59:28 +0100

    Configure esbuild

--- apps/api/build.mjs
+++ apps/api/build.mjs
@@ -2,10 +2,10 @@ import * as esbuild from "esbuild";
 
 await esbuild.build({
   entryPoints: ["./src/server.ts"],
-  outfile: "./dist/server.js",
-  bundle: true,
+  outdir: "dist",
   platform: "node",
+  format: "esm",
   target: "es2023",
-  sourcemap: true,
-  minify: false,
+  bundle: true,
+  packages: "external",
 });
